038b054f7e9e27ee4153aa4a8908e886ae60661d
Anna <trzeciakanna@hotmail.com>
2018-05-20 21:03:51 +0200

    header and about section

---
+++ index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="pl">
+  <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <link rel="stylesheet" href="style/bootstrap.css">
+      <link rel="stylesheet" href="style/style.css">
+      <title>John Smith - Web Designer</title>
+  </head>
+  <body>
+    <header class="d-flex justify-content-center align-items-center">
+    <nav class="navbar navbar-expand-lg fixed-top">
+      <div class="container">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav d-flex justify-content-around">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item nav-link">-</li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item nav-link">-</li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Our services</a>
+            </li>
+            <li class="nav-item nav-link">-</li>
+            <li class="nav-item">
+              <a class="nav-link " href="#">Portfolio</a>
+            </li>
+            <li class="nav-item nav-link">-</li>
+            <li class="nav-item">
+              <a class="nav-link " href="#">Experience</a>
+            </li>
+              <li class="nav-item nav-link">-</li>
+            <li class="nav-item">
+              <a class="nav-link " href="#">Blog</a>
+            </li>
+            <li class="nav-item nav-link">-</li>
+            <li class="nav-item">
+              <a class="nav-link " href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    <div class="header-content">
+      <h3>Hello, I am</h3>
+      <h1><span class="text-gradient">John</span><br><span>Smith</span></h1>
+      <h4>I have more than 10 years of experience</h4>
+      <a style="margin-top: 50px"href="#" class="btn btn-gradient"><span>Download CV</span></a>
+    </div>
+    </header>
+    <section class="about-section">
+      <div class="container">
+        <div class="row">
+          <div class=" d-flex justify-content-end col-md-4 text-right flex-column">
+            <h2>John Smith</h2>
+            <span class="text-purpure text-uppercase font-bold">Web Designer</span>
+            <p >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+          </div>
+          <div class="col-md-5 px-0 d-flex align-items-end">
+            <img src="images/john-smith.png" alt="John Smith" class="img-fluid">
+          </div>
+          <div class="col-md-3 d-flex align-items-end flex-column">
+            <div class="skills">
+              <div class="skill">
+                <p class="text-center">Design (80%)</p>
+
+                <div class="graph" style="background-image: linear-gradient(45deg, #a21af0 0%, #a21af0 78%, #ff0072 82%, #ff0072 100%);">
+                </div>
+              </div>
+              <div class="skill">
+                <p class="text-center">Photography (65%)</p>
+                <div class="graph" style="background-image: linear-gradient(45deg, #a21af0 0%, #a21af0 63%, #ff0072 67%, #ff0072 100%);">
+
+                </div>
+              </div>
+              <div class="skill">
+                <p class="text-center">Marketing (50%)</p>
+                <div class="graph" style="background-image: linear-gradient(45deg, #a21af0 0%, #a21af0 78%, #ff0072 82%, #ff0072 100%);">
+
+                </div>
+              </div>
+              <div class="skill">
+                <p class="text-center">Branding (30%)</p>
+                <div class="graph" style="background-image: linear-gradient(45deg, #a21af0 0%, #a21af0 28%, #ff0072 32%, #ff0072 100%);">
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="contact-section">
+        <div class="container">
+          <div class="row">
+
+          <div class="col-md-4">
+            <div class="row">
+            <div class="col-4">
+              <i class="phone"></i>
+            </div>
+            <div class="col-8">
+              <h5>Phone number</h5>
+              <span>+48 123 123 123</span><br>
+              <span>+48 987 654 321</span>
+            </div></div>
+          </div>
+          <div class="col-md-4">
+            <div class="row">
+            <div class="col-4">
+              <i class="localization"></i>
+            </div>
+            <div class="col-8">
+              <h5>Address</h5>
+              <span>Al. Jerozolimskie 160</span><br>
+              <span>01-234 Warszawa</span>
+            </div>
+          </div></div>
+          <div class="col-md-4">
+            <div class="row">
+
+            <div class="col-4">
+              <i class="mail"></i>
+            </div>
+            <div class="col-8">
+              <h5>Email</h5>
+              <span>john.smith@company.pl</span><br>
+              <span>office@company.pl</span>
+            </div></div>
+          </div>
+</div>
+        </div>
+
+    </section>
+    <script src="js/jquery-3.3.1.js"></script>
+    <script src="js/popper.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
+  </body>
+
+</html>
