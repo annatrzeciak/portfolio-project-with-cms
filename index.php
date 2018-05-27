f47d157a52ab622121bee62729c2ff05971a2913
Anna <trzeciakanna@hotmail.com>
2018-05-27 22:24:56 +0200

    add portfolio, blog and footer section

--- index.php
+++ index.php
@@ -3,13 +3,15 @@
   <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <link rel="stylesheet" href="style/bootstrap.css">
-      <link rel="stylesheet" href="style/style.css">
+      <link rel="stylesheet" href="./style/bootstrap.css">
+      <link rel="stylesheet" type="text/css" href="./slick/slick/slick.css" />
+      <link rel="stylesheet" type="text/css" href="./slick/slick/slick-theme.css" />
+      <link rel="stylesheet" href="./style/style.css">
       <title>John Smith - Web Designer</title>
   </head>
   <body>
     <header class="d-flex justify-content-center align-items-center">
-    <nav class="navbar navbar-expand-lg fixed-top">
+    <nav class="navbar navbar-expand-lg fixed-top navbar-dark">
       <div class="container">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -58,15 +60,15 @@
     <section class="about-section">
       <div class="container">
         <div class="row">
-          <div class=" d-flex justify-content-end col-md-4 text-right flex-column">
+          <div class=" py-5 d-flex justify-content-end col-lg-4 text-right flex-column">
             <h2>John Smith</h2>
             <span class="text-purpure text-uppercase font-bold">Web Designer</span>
             <p >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
           </div>
-          <div class="col-md-5 px-0 d-flex align-items-end">
+          <div class=" col-lg-5 col-md-7 px-0 d-flex align-items-end">
             <img src="images/john-smith.png" alt="John Smith" class="img-fluid">
           </div>
-          <div class="col-md-3 d-flex align-items-end flex-column">
+          <div class="py-5 col-lg-3 col-md-5 d-flex align-items-end ">
             <div class="skills">
               <div class="skill">
                 <p class="text-center">Design (80%)</p>
@@ -82,7 +84,7 @@
               </div>
               <div class="skill">
                 <p class="text-center">Marketing (50%)</p>
-                <div class="graph" style="background-image: linear-gradient(45deg, #a21af0 0%, #a21af0 78%, #ff0072 82%, #ff0072 100%);">
+                <div class="graph" style="background-image: linear-gradient(45deg, #a21af0 0%, #a21af0 48%, #ff0072 52%, #ff0072 100%);">
 
                 </div>
               </div>
@@ -97,11 +99,11 @@
         </div>
       </div>
     </section>
-    <section class="contact-section">
+    <section class="contact-section page-section">
         <div class="container">
           <div class="row">
 
-          <div class="col-md-4">
+          <div class="col-lg-4">
             <div class="row">
             <div class="col-4">
               <i class="phone"></i>
@@ -112,7 +114,7 @@
               <span>+48 987 654 321</span>
             </div></div>
           </div>
-          <div class="col-md-4">
+          <div class="col-lg-4">
             <div class="row">
             <div class="col-4">
               <i class="localization"></i>
@@ -123,7 +125,8 @@
               <span>01-234 Warszawa</span>
             </div>
           </div></div>
-          <div class="col-md-4">
+          <div class="col-lg-4">
+
             <div class="row">
 
             <div class="col-4">
@@ -135,14 +138,171 @@
               <span>office@company.pl</span>
             </div></div>
           </div>
-</div>
+        </div>
+      </div>
+    </section>
+    <section class="portfolio-section page-section">
+      <div class="container">
+        <h3 class="section-title">Portfolio</h3>
+        <p class="text-center">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.</p>
+        <div class="portfolio-items row">
+          <div class="col-md-6 ">
+            <div class="portfolio-item">
+               <img src="./images/portfolio-business-card.png" alt="" class="img-fluid">
+            </div>
+            <div class="portfolio-item">
+              <img src="./images/portfolio-cv-template.png" alt="" class="img-fluid">
+            </div>
+            <div class="portfolio-item">
+              <img src="./images/portfolio-nature-template.png" alt=""class="img-fluid" >
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="portfolio-item">
+              <img src="./images/portfolio-restaurant-table.png" alt=""class="img-fluid" >
+            </div>
+            <div class="portfolio-item">
+              <img src="./images/portfolio-summer.png" alt=""class="img-fluid" >
+            </div>
+            <div class="portfolio-item">
+              <img src="./images/portfolio-website-template.png" alt="" class="img-fluid">
+          </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="comments page-section">
+      <div class="container">
+        <h3 class="section-title">Comments</h3>
+        <div class="slider">
+          <div class="comment-item item">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="quotation">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </div>
+              </div>
+              <div class="col-md-5 offset-md-1">
+                <div class="row d-flex align-items-center">
+                  <div class="col-5">
+                      <img src="./images/Ann-Brown.png">
+                  </div>
+                  <div class="col-7">
+                    <h4>Ann Brown</h4>
+                    <span>Graphic Designer</span>
+                  </div>
+                </div>
+              </div>
+            </div></div>
+            <div class="comment-item item">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="quotation">
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </div>
+                </div>
+                <div class="col-md-5 offset-md-1">
+                  <div class="row d-flex align-items-center">
+                    <div class="col-5">
+                        <img src="./images/Ann-Brown.png">
+                    </div>
+                    <div class="col-7">
+                      <h4>John Snow</h4>
+                      <span>company.com</span>
+                    </div>
+                  </div>
+                </div>
+              </div></div>
+              <div class="comment-item item">
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="quotation">
+                      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </div>
+                  </div>
+                  <div class="col-md-5 offset-md-1">
+                    <div class="row d-flex align-items-center">
+                      <div class="col-5">
+                          <img src="./images/Ann-Brown.png">
+                      </div>
+                      <div class="col-7">
+                        <h4>Alice Foster</h4>
+                        <span>factory-name.com</span>
+                      </div>
+                    </div>
+                  </div>
+                </div></div>
+                <div class="comment-item item">
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="quotation">
+                      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                      </div>
+                    </div>
+                    <div class="col-md-5 offset-md-1">
+                      <div class="row d-flex align-items-center">
+                        <div class="col-5">
+                            <img src="./images/Ann-Brown.png">
+                        </div>
+                        <div class="col-7">
+                          <h4>John Kowalski </h4>
+                          <span>abc.com</span>
+                        </div>
+
+                      </div>
+                    </div>
+                  </div>
+              </div>
+            </div>
+        </div>
+    </section>
+    <section class="page-section blog-section">
+      <div class="container">
+        <h3 class="section-title">Blog</h3>
+        <p class="text-center">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="row">
+          <a href='#' class="col-md-4 short-blog-text">
+            <img src="./images/woman.png" alt="" class="img-fluid">
+            <span class="blog-category">Personal Adviser</span>
+            <h5>Computer and using some color</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+          </a>
+            <a href='#'  class="col-md-4 short-blog-text">
+
+            <img src="./images/building.png" alt="" class="img-fluid">
+
+            <span class="blog-category">Personal Adviser</span>
+            <h5>Computer and using some color</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+          </a>
+            <a href='#'  class="col-md-4 short-blog-text">
+
+            <img src="./images/person.png" alt="" class="img-fluid">
+
+            <span class="blog-category">Personal Adviser</span>
+            <h5>Computer and using some color</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+          </a>
+
         </div>
 
+      </div>
+
     </section>
-    <script src="js/jquery-3.3.1.js"></script>
-    <script src="js/popper.js"></script>
-    <script src="js/bootstrap.js"></script>
-    <script type="text/javascript" src="js/script.js"></script>
+    <footer class="footer">
+      <div class="container">
+        <div class="col-12 text-center">
+          Copyright 2018. All rights reserved.
+        </div>
+
+      </div>
+    </footer>
+
+    <script src="./js/jquery-3.3.1.js"></script>
+    <script src="./js/popper.js"></script>
+    <script src="./js/bootstrap.js"></script>
+    <script type="text/javascript" src="./slick/slick/slick.js"></script>
+    <script type="text/javascript" src="./js/script.js"></script>
   </body>
 
 </html>
--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ $('a').click(function(){
     }, 500);
     return false;
 });
+$(document).ready(function () {
+    $('.slider').slick({
+        dots: false,
+        arrow: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        nextArrow: '<i class="arrow arrow-right"></i>',
+        prevArrow: '<i class="arrow arrow-left"></i>',
+        slidesToShow: 1
+    });
+});
